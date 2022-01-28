4ac45c7f8cea948f3bb6f88fa47eba2e5e6a44f1
bmahmoudyan <benyaminmahmoudyan@gmail.com>
2022-01-28 15:10:59 +0330

    delete function javascript

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,22 @@
 let input = document.getElementById("task");
 let ul = document.getElementById("list");
 
+function deleteItem(event) {
+    event.target.parentElement.remove();
+}
+
 function addTask() {
     let inputValue = input.value.trim();
-    if(inputValue.lenght > 0) {
+    input.value = "";
+    if(inputValue.length > 0) {
         let li = document.createElement('li');
-        li.appendChild(document.createTextNode(input.value));
-        input.value = "";
+        li.appendChild(document.createTextNode(inputValue));
+        let deleteIcone = document.createElement('i');
+        deleteIcone.classList.add("fas", "fa-trash", "delete-icone");
+        li.appendChild(deleteIcone);
         ul.appendChild(li);
+        deleteIcone.addEventListener("click", deleteItem);
     } else {
-        input.value = '';
         alert('you have to write something');
     }
 }
@@ -19,4 +26,6 @@ function addTaskWithEnter(event) {
         addTask();
     }
 }
+
 input.addEventListener('keypress', addTaskWithEnter)
+
